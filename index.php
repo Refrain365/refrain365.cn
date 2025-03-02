bc2d837c39055ddcc4ad6100ace80f70271503c0
Refrain365 <3047278723@qq.com>
2025-03-02 14:11:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php $IP = $_SERVER['SERVER_ADDR'];?>
+<?php $URL = $_SERVER['HTTP_HOST'];?>
+<meta name="referrer" content="never">
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="renderer" content="webkit">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<title>真理的中点</title>
+	</head>
+	<body>
+		<div id="wrap">
+				<div id="logo">
+					<img class="logo" src="img/logo.png">
+					<!-- <h1>真理的中点</h1> -->
+					<div id="kg-btn" class="">
+						<input class='tgl tgl-flip' id='qieh' type='checkbox' />
+						<label class='tgl-btn' data-tg-off='中文' data-tg-on='English' for='qieh'></label>
+				</div>
+			</div>
+			<div id="main">
+			<!-- 外网访问地址 -->
+				<div id="app" class="app animated fadeInLeft">
+					<body>
+						<p><b>·公告：</b>本站已更新至1.0.6版本，详情请见<a href =/>更新概况<a> (2024.7.21)</p>
+					</body>
+					<ul>
+						<li>
+							<a href="/ariang/" target=""><img class="shake" src="img/png/AriaNg.png" /><strong>离线下载</strong></a>
+						</li>
+						<li>
+							<a href="/" target=""><img class="shake" src="img/png/kod.png" /><strong>可道云</strong></a>
+						</li>
+						<li>
+						    <a href="/apps/" target=""><img class="shake" src="img/png/apps.png" /><strong>应用</strong></a>
+						</li>
+						<li>
+							<a href="/" target=""><img class="shake" src="img/png/wordpress.png" /><strong>论坛</strong></a>
+						</li>
+						<li>
+							<a href="/" target=""><img class="shake" src="img/png/source.png" /><strong>资源</strong></a>
+						</li>
+						<li>
+							<a href="/" target="_blank"><img class="shake" src="img/png/Podcats.png" /><strong>后台管理</strong></a>
+						</li>
+						<li>
+							<a href="/about/" target=""><img class="shake" src="img/png/about.png" /><strong>关于</strong></a>
+						</li>
+					</ul>
+				</div>
+				<!-- 内网网访问地址 -->
+				<div id="app1" class="app animated fadeInRight">
+					<body>
+						<p><b>·Announcement:</b>This website has been updated to version 1.0.6. Please refer to the <a href =/>update overview for details.<a></p>
+					</body>
+					<ul>
+						<li>
+							<a href="/ariang/" target="_blank"><img class="shake" src="img/png/AriaNg.png" /><strong>download</strong></a>
+						</li>
+						<li>
+							<a href="/" target="_blank"><img class="shake" src="img/png/kod.png" /><strong>kod</strong></a>
+						</li>
+						<li>
+						    <a href="/apps/" target="_blank"><img class="shake" src="img/png/apps.png" /><strong>apps</strong></a>
+						</li>
+						<li>
+							<a href="/" target="_blank"><img class="shake" src="img/png/wordpress.png" /><strong>forum</strong></a>
+						</li>
+						<li>
+							<a href="/" target="_blank"><img class="shake" src="img/png/source.png" /><strong>source</strong></a>
+						</li>
+						<li>
+							<a href="/" target="_blank"><img class="shake" src="img/png/Podcats.png" /><strong>WP config</strong></a>
+						</li>
+						<li>
+							<a href="/about/" target="_blank"><img class="shake" src="img/png/about.png" /><strong>about</strong></a>
+						</li>
+					</ul>
+				</div>
+				<div style="clear: both;"></div>
+			</div>
+		</div>
+		<script src="js/jquery.min.js"></script>
+	</body>
+</html>
