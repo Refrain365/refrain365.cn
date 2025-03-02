2cee05410792596487d8af4c1e4b1616af04144c
Refrain365 <3047278723@qq.com>
2025-03-02 14:20:27 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -10,76 +10,86 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="shortcut icon" href="/favicon.ico">
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
-		<title>真理的中点</title>
+		<title>服务器/关于</title>
 	</head>
 	<body>
 		<div id="wrap">
 				<div id="logo">
 					<img class="logo" src="img/logo.png">
-					<!-- <h1>真理的中点</h1> -->
+					<!-- <h1>服务器/关于</h1> -->
 					<div id="kg-btn" class="">
 						<input class='tgl tgl-flip' id='qieh' type='checkbox' />
 						<label class='tgl-btn' data-tg-off='中文' data-tg-on='English' for='qieh'></label>
+					<br>
+					<a href ='/'>返回</a>
 				</div>
 			</div>
 			<div id="main">
 			<!-- 外网访问地址 -->
 				<div id="app" class="app animated fadeInLeft">
-					<body>
-						<p><b>·公告：</b>本站已更新至1.0.6版本，详情请见<a href =/>更新概况<a> (2024.7.21)</p>
-					</body>
-					<ul>
-						<li>
-							<a href="/ariang/" target=""><img class="shake" src="img/png/AriaNg.png" /><strong>离线下载</strong></a>
-						</li>
-						<li>
-							<a href="/" target=""><img class="shake" src="img/png/kod.png" /><strong>可道云</strong></a>
-						</li>
-						<li>
-						    <a href="/apps/" target=""><img class="shake" src="img/png/apps.png" /><strong>应用</strong></a>
-						</li>
-						<li>
-							<a href="/" target=""><img class="shake" src="img/png/wordpress.png" /><strong>论坛</strong></a>
-						</li>
-						<li>
-							<a href="/" target=""><img class="shake" src="img/png/source.png" /><strong>资源</strong></a>
-						</li>
-						<li>
-							<a href="/" target="_blank"><img class="shake" src="img/png/Podcats.png" /><strong>后台管理</strong></a>
-						</li>
-						<li>
-							<a href="/about/" target=""><img class="shake" src="img/png/about.png" /><strong>关于</strong></a>
-						</li>
-					</ul>
+			<body>
+				<h2>关于</h2>
+					<br>
+					<p>网站信息：
+					</p>
+					<p>版本号：1.0.6</p>
+					<p>虚拟网络IP地址：10.147.17.213</p>
+					<br>
+					<p>服务器信息：
+					</p>
+					<p>系统：Windows 11 23H2</p>
+					<p>CPU：6核心12线程，2.90GHz</p>
+					<p>内存：DDR4，单通道8GB</p>
+					<p>机械硬盘：1000GB（服务器使用620GB）</p>
+					<p>主板制造商：Colorful（七彩虹）</p>
+					<br>
+					<p>运行环境及应用版本信息：
+					</p>
+					<p>MySQL版本：5.7.26</p>
+					<p>php版本：7.3.4nts</p>
+					<p>Nginx版本：1.5.11</p>
+					<p>redis版本：3.0.504</p>
+					<p>可道云版本：1.52.02</p>
+					<p>论坛(Z-Blog)版本：1.7.3.3290 (Finch)</p>
+					<br>
+					<p><b>Shared with a bright future.<b></p>
+					<p><a href ='/kodbox/#admin/server'>运行状态</a>		<a href ='/wordpress/?id=8'>用户须知</a></p>
+					<p><a href ='/wordpress/?id=2'>问题反馈</a>		<a href ='https://space.bilibili.com/1091296209'>作者主页</a>
+			</body>
 				</div>
 				<!-- 内网网访问地址 -->
 				<div id="app1" class="app animated fadeInRight">
-					<body>
-						<p><b>·Announcement:</b>This website has been updated to version 1.0.6. Please refer to the <a href =/>update overview for details.<a></p>
-					</body>
-					<ul>
-						<li>
-							<a href="/ariang/" target="_blank"><img class="shake" src="img/png/AriaNg.png" /><strong>download</strong></a>
-						</li>
-						<li>
-							<a href="/" target="_blank"><img class="shake" src="img/png/kod.png" /><strong>kod</strong></a>
-						</li>
-						<li>
-						    <a href="/apps/" target="_blank"><img class="shake" src="img/png/apps.png" /><strong>apps</strong></a>
-						</li>
-						<li>
-							<a href="/" target="_blank"><img class="shake" src="img/png/wordpress.png" /><strong>forum</strong></a>
-						</li>
-						<li>
-							<a href="/" target="_blank"><img class="shake" src="img/png/source.png" /><strong>source</strong></a>
-						</li>
-						<li>
-							<a href="/" target="_blank"><img class="shake" src="img/png/Podcats.png" /><strong>WP config</strong></a>
-						</li>
-						<li>
-							<a href="/about/" target="_blank"><img class="shake" src="img/png/about.png" /><strong>about</strong></a>
-						</li>
-					</ul>
+			<body>
+				<h2>About This Server</h2>
+					<br>
+					<p>Website info：
+					</p>
+					<p>Version：1.0.6</p>
+					<p>Virtual network IP address：10.147.17.213</p>
+					<br>
+					<p>Server info：
+					</p>
+					<p>Operating system：Windows 11 23H2</p>
+					<p>CPU：6cores,12threads，2.90GHz</p>
+					<p>RAM：DDR4，single channel 8GB</p>
+					<p>ROM：1000GB（Server usage:620GB）</p>
+					<p>Motherboard company：Colorful</p>
+					<br>
+					<p>Operating environment&application ver. info：
+					</p>
+					<p>MySQL version：5.7.26</p>
+					<p>php version：7.3.4nts</p>
+					<p>Nginx version：1.5.11</p>
+					<p>redis version：3.0.504</p>
+					<p>Kodbox version：1.52.02</p>
+					<p>Z-Blog version：1.7.3.3290 (Finch)</p>
+					<br>
+					<p><b>Shared with a bright future.<b></p>
+					<p><a href ='/kodbox/#admin/server'>Status</a></p>
+					<p><a href ='/wordpress/?id=8'>Notice</a></p>
+					<p><a href ='/wordpress/?id=2'>Faq</a></p>
+					<p><a href ='https://space.bilibili.com/1091296209'>AuthorPage</a></p>
+			</body>
 				</div>
 				<div style="clear: both;"></div>
 			</div>
